72b09b3f760f60ec540253e7f20f022d82a540e6
NeAndrew <65078011+Andrey1233-maker@users.noreply.github.com>
2022-12-24 15:06:30 +0300

    Saturn

--- routes/templates/scripts/profile.js
+++ routes/templates/scripts/profile.js
@@ -10,7 +10,7 @@ async function getUserByEmail() {
                 ['Content-Type']: 'application/json',
                 ['charset']: 'utf-8',
                 ['Authorization'] : `Bearer ${localStorage.getItem('token')}`,
-            },
+            },  
             }) 
             .then((response) => response.json())
             .then((data) => setProfilee(data.user))  
@@ -40,4 +40,46 @@ function setProfilee(user) {
     document.getElementById('user_desc_p').textContent = user.favorite_car
 }
 
+async function getStates() {
+    try {
+        let params = (new URL(document.location)).searchParams
+        console.log(params.get("email"))
+        if(params.get("email")) {
+        await fetch('http://localhost:3000/state?email=', {
+            method: 'GET',
+            headers: {
+                ['Content-Type']: 'application/json',
+                ['charset']: 'utf-8',
+                ['Authorization'] : `Bearer ${localStorage.getItem('token')}`,
+            },
+        }).then((response) => response.json())
+        .then((data) => {
+            if(data.states){
+            data.states.map((state) => {
+                const div = document.createElement('div')
+                div.className = 'list_item'
+                div.innerHTML= `<img src="http://localhost:3000/img/driver.png" alt="state_logo" class="state_logo"/>
+                                <div>
+                                    <p class="state_title">${state.name}</p>
+                                    <div class="block8"></div>
+                                    <p class="state_text">${state.private ? 'Приватная статья' : 'Публичная статья'}</p>
+                                    <div class="block8"></div>
+                                    <p class="state_text">Автор: <span  class="state_value">${state.autor}</span></p>
+                                </div>`
+                document.getElementById('states_list').append(div)
+            })
+            } else {
+                const div = document.createElement('div')
+                div.innerHTML = '<h1>Статей нет</h1>'
+                document.getElementById('states_list').append(div)
+            }
+        })
+    }
+}
+    catch(e) {
+        throw e
+    }
+}
+
 getUserByEmail()
+getStates()
